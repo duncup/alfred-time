dff37f3efa41a40c988467719265bfd2fa4a1854
duncup <dlmsoft.wolf@gmail.com>
2019-12-15 17:55:42 +0800

    feat(*): New features: time string generator

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const moment = require('moment');
 const alfredNotifier = require('alfred-notifier');
 
 alfredNotifier();
+const time = moment();
 
 alfy.output([
 	{
-		title: 'Unicorn',
-		subtitle: alfy.input
-	}
+		title: '时间字符串',
+		subtitle: time.format(alfy.input),
+		arg: time.format(alfy.input),
+	},
+	{
+		title: '时间字符串-本地格式',
+		subtitle: time.local(),
+		arg: time.local(),
+	},
+	{
+		title: '时间字符串-常见格式1',
+		subtitle: time.format('YYYY-MM-DD HH:mm:ss'),
+		arg: time.format('YYYY-MM-DD HH:mm:ss'),
+	},
+	{
+		title: '时间字符串-常见格式2',
+		subtitle: time.format('YYYY/MM/DD HH:mm:ss'),
+		arg: time.format('YYYY/MM/DD HH:mm:ss'),
+	},
 ]);
